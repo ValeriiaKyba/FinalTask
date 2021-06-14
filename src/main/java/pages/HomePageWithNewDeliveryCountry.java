a281aa0af59f088bfa0005370d5ba9e07b592f7b
ValeriiaKyba <valeriia.kyba@gmail.com>
2021-06-14 19:28:24 +0300

    Updated Final Task

--- src/main/java/pages/HomePageWithNewDeliveryCountry.java
+++ src/main/java/pages/HomePageWithNewDeliveryCountry.java
@@ -6,17 +6,13 @@ import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
 
-public class HomePageWithNewDeliveryCountry extends BasePage{
+public class HomePageWithNewDeliveryCountry extends HomePage{
 
     @FindBy(xpath = "//span[contains(@id, 'glow-ingress-line')]")
     private List<WebElement> deliveryCountryInformation;
 
     public HomePageWithNewDeliveryCountry(WebDriver driver) {
         super(driver);
-    }
-
-    public void openHomePage(final String url) {
-        driver.get(url);
     }
 
     public String getDeliveryCountryName() {
